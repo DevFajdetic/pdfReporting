da4352aa220cf4919c28f5eb5ca1da743b14f59a
DevFajdetic <kristijanfajdeticcro@gmail.com>
2023-01-06 01:08:54 +0100

    Structural changes - helpers package, examples for input ex and products for outputs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import calendar
 from datetime import datetime
-from fpdf import FPDF
+from helpers import pdfService
 
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
@@ -81,48 +81,6 @@ def construct():
     return [*pages_data, temp]
 
 
-class PDF(FPDF):
-    def __init__(self):
-        super().__init__()
-        self.WIDTH = 210
-        self.HEIGHT = 297
-
-    def header(self):
-        # Custom logo and positioning
-        # Create an `assets` folder and put any wide and short image inside
-        # Name the image `logo.jpg`
-        self.image('assets/logo.jpg', 10, 8, 33)
-        self.set_font('Arial', 'B', 11)
-        self.cell(self.WIDTH - 80)
-        self.cell(60, 1, 'Sales report', 0, 0, 'R')
-        self.ln(20)
-
-    def footer(self):
-        # Page numbers in the footer
-        self.set_y(-15)
-        self.set_font('Arial', 'I', 8)
-        self.set_text_color(128)
-        self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
-
-    def page_body(self, images):
-        # Determine how many plots there are per page and set positions
-        # and margins accordingly
-        if len(images) == 3:
-            self.image(images[0], 15, 25, self.WIDTH - 30)
-            self.image(images[1], 15, self.WIDTH / 2 + 5, self.WIDTH - 30)
-            self.image(images[2], 15, self.WIDTH / 2 + 90, self.WIDTH - 30)
-        elif len(images) == 2:
-            self.image(images[0], 15, 25, self.WIDTH - 30)
-            self.image(images[1], 15, self.WIDTH / 2 + 5, self.WIDTH - 30)
-        else:
-            self.image(images[0], 15, 25, self.WIDTH - 30)
-
-    def print_page(self, images):
-        # Generates the report
-        self.add_page()
-        self.page_body(images)
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     generate_sales_data(month=3)
@@ -130,9 +88,9 @@ if __name__ == '__main__':
     plot(data=december, filename='december.png')
     plots_per_page = construct()
     plots_per_page
-    pdf = PDF()
+    pdf = pdfService.PDF()
 
     for elem in plots_per_page:
         pdf.print_page(elem)
 
-    pdf.output('SalesRepot.pdf', 'F')
+    pdf.output('products/SalesReport.pdf', 'F')
